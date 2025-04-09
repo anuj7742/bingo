6b861d8c119d54f55f12d2a499b81a90facde88a
Anuj Kumawat <144904655+anuj7742@users.noreply.github.com>
2025-04-09 12:05:44 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const socket = io('http://localhost:4000');
+const socket = io(process.env.Back_url);
 
 socket.on('connect-error', (err) => {
     console.error(`connection error ${err.message}`)
