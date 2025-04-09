211f8c2c63868cd30478dc2eefd4e99b8ed2f413
anuj7742 <anujkumawat7742@gmail.com>
2025-04-09 12:34:01 +0530

    Minor fixes

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,7 @@
 import {io} from 'socket.io-client'
 
-<<<<<<< HEAD
 const socket = io(process.env.REACT_APP_BACK_URL);
-=======
-const socket = io(process.env.Back_url);
->>>>>>> dbf440180cc3afb79a472e767161cc93802fe4ed
+
 
 socket.on('connect-error', (err) => {
     console.error(`connection error ${err.message}`)
